6dcb4ff6ef0430797947643eed79ec15153ec481
Personal <ander5494@gmail.com>
2018-11-20 11:11:20 +0100

    exceptionCreation

--- exec/install.py
+++ exec/install.py
@@ -1,11 +1,15 @@
 import mysql.connector
+try:
+	db = mysql.connector.connect(
+		host="localhost"
+		user="root"
+		passwd="root"
+		)
 
-db = mysql.connector.connect(
-	host="localhost"
-	user="root"
-	passwd="root"
-	)
+	cursor= db.cursor()
 
-cursor= db.cursor()
-
-cursor.execute("Create database prueba")
+	cursor.execute("Create database prueba")
+except ConnectionError as e:
+	print("Can't connect to DB")
+except Exception as e:
+	print(e)
