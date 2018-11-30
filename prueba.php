980249bf849c962283a7293373e1a09b62ff6408
igor-dataka <igor.b@data-ka.com>
2018-11-30 16:05:43 +0100

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -1,13 +1,9 @@
 <?php
-$i=1;
 if (isset($_GET['env'])){
     $env=$_GET['env'];
 }
 include ("model/header.php");
-?>
-
-<div id="principal">
-    <?php
+echo('<div id="principal">');
         include ("model/carrousel.php");
         include("model/columns.php");
         include ("model/login.html");
